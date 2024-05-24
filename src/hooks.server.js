c410149bcd0516862b1682b32cfd77ad8bc18950
NabilTouri <tourinabil3@gmail.com>
2024-05-24 23:32:29 +0200

    fix issues

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,43 +1,22 @@
-// import { DB_USER, DB_HOST, DB_PASSWORD, DB_DATABASE, DB_PORT, CA_CERT } from '$env/static/private'
-// import mysql from 'mysql2/promise';
-
-// let mysqlconn = null;
-
-// export function mysqlconnFn() {
-
-//     if (!mysqlconn) {
-//         mysqlconn = mysql.createConnection({ 
-//             host: DB_HOST,
-//             user: DB_USER,
-//             password: DB_PASSWORD,
-//             database: DB_DATABASE,
-//             port: DB_PORT, // Imposta la porta corretta
-//             ssl: {
-//                 ca: CA_CERT, // Inserisci il certificato CA fornito
-//                 rejectUnauthorized: true // Imposta a true se vuoi che la connessione venga rifiutata se il certificato non è valido
-//             }
-//         });
-//     }
-    
-//     return mysqlconn;
-// }
-
-
-import { DB_USER, DB_HOST, DB_PASSWORD, DB_DATABASE } from '$env/static/private'
+import { DB_USER, DB_HOST, DB_PASSWORD, DB_DATABASE, DB_PORT, CA_CERT } from '$env/static/private'
 import mysql from 'mysql2/promise';
 
 let mysqlconn = null;
 
 export function mysqlconnFn() {
-
     if (!mysqlconn) {
         mysqlconn = mysql.createConnection({ 
             host: DB_HOST,
-            user:  DB_USER,
+            user: DB_USER,
             password: DB_PASSWORD,
-            database: DB_DATABASE
+            database: DB_DATABASE,
+            port: DB_PORT, // Imposta la porta corretta
+            ssl: {
+                ca: CA_CERT, // Inserisci il certificato CA fornito
+                rejectUnauthorized: true // Imposta a true se vuoi che la connessione venga rifiutata se il certificato non è valido
+            }
         });
-    }
-
+     
     return mysqlconn;
+    }
 }
