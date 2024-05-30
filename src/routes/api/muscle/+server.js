b03d2fbfc6fa2c27ceaf5191f7add75309514791
NabilTouri <tourinabil3@gmail.com>
2024-05-30 10:56:43 +0200

    insert expert exercises

--- src/routes/api/muscle/+server.js
+++ src/routes/api/muscle/+server.js
@@ -26,36 +26,29 @@ export const GET = async ({ fetch }) => {
         'triceps',
     ];
 
-    console.log(muscles);
-
     const allexercises = [];
 
-    for (const muscle of muscles) {
-        try {
-            const response = await fetch(`https://api.api-ninjas.com/v1/exercises?muscle=${muscle}`, {
-                headers: {
-                    'X-Api-Key': 'mdSFB21Rg1wlfhJmUeT+aQ==Sq8gwUUGDxp8NOYB'
-                }
-            });
-
-            if (!response.ok) {
-                console.error(`Failed to fetch data for muscle: ${muscle}, Status: ${response.status}`);
-            } else {
-                console.log(`Fetched data for muscle: ${muscle}`);
-                const exercises = await response.json();
-                allexercises.push(...exercises);
+    try {
+        const response = await fetch(`https://api.api-ninjas.com/v1/exercises?difficulty=expert`, {
+            headers: {
+                'X-Api-Key': 'mdSFB21Rg1wlfhJmUeT+aQ==Sq8gwUUGDxp8NOYB'
             }
-
-            // Introduce a delay of 1 second between requests
-            await delay(1000);
-        } catch (error) {
-            console.error(`Error fetching data for muscle: ${muscle}, Error: ${error.message}`);
+        });
+        if (!response.ok) {
+            console.error(`Failed to fetch data, Status: ${response.status}`);
+        } else {
+            console.log(`Fetched data`);
+            const exercises = await response.json();
+            allexercises.push(exercises);
+            console.log(exercises);
         }
+    } catch (error) {
+        console.error(`Error fetching data, Error: ${error.message}`);
     }
+    
 
-    // Insert the exercises into the database
-    const insertExercisesPromises = allexercises.map(async (exercise) => {
-        try {
+    try {
+        allexercises.forEach(async (exercise) => {
             const { name, type, muscle, equipment, difficulty, instructions } = exercise;
             const query = `
                 INSERT INTO exercises (name, type, muscle, equipment, difficulty, instructions)
@@ -70,12 +63,12 @@ export const GET = async ({ fetch }) => {
 
             await mysqlconn.query(query, [name, type, muscle, equipment, difficulty, instructions]);
             console.log(`Inserted exercise: ${exercise.name}`);
-        } catch (error) {
-            console.error(`Error inserting exercise: ${exercise.name}, Error: ${error.message}`);
-        }
-    });
+        });
+    }
+    catch (error) {
+        console.error(`Error inserting data, Error: ${error.message}`);
+    }
 
-    await Promise.all(insertExercisesPromises);
 
     return json({ message: 'Exercises inserted into the database successfully', exercises: allexercises });
 };
